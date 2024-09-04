c45630177bc09fdf456680be7121377205a38c5e
Aarav Raina <rainaaarav05@gmail.com>
2024-09-04 13:35:29 +0530

    Image Cards stacked on Top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import Component1 from "@/components/sections/Component1";
 
+import Component1 from "@/components/sections/Component1";
+import StickyStack from '@/components/StickyStack';
 export default function Home() {
+
   return (
     <main>
       <Component1 />
+      <StickyStack />
     </main>
   );
 }
+
