692ba142e62859a80c63b3a72369d5e02d56e345
vaibhav700c <vaibhavjangid128@gmail.com>
2024-09-05 13:00:37 +0530

    new event page created
    Created a bew event page using 2d card and now will just have to make some changes to make it look good

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
 import StickyStack from '@/components/StickyStack';
-import MarqueeComponent from '../components/MarqueeComponent';
 import EventPage from "@/components/sections/Events/eventpage";
+import { ThreeDCardDemo } from '@/components/ui/Card3d';
+import MarqueeComponent from '../components/MarqueeComponent';
 
 export default function Home() {
 
   return (
     <main>
+      <EventPage />
       <StickyStack />
       <MarqueeComponent />
-      
-      <EventPage />
-   
     </main>
   );
 }
