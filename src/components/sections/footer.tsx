a4e97d726855fb4b91f233dce4e3665f95b81dc3
Raunak <raunakzyx2@gmail.com>
2024-09-05 08:07:26 +0530

    added navbar and footer coomponent

--- src/components/sections/footer.tsx
+++ src/components/sections/footer.tsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <div>
-      {/* Top Section with Logo and Buttons */}
+      
       <div className="h-[40vh] max-h-[40vh] bg-black flex items-center justify-between px-40">
-        {/* Logo Section */}
+        
         <div className="flex-shrink-0 ml-10">
           <Image
             className="Codechef_Logo"
@@ -17,9 +17,9 @@ const Footer = () => {
           />
         </div>
 
-        {/* Button Section */}
+        
         <div className="ml-20 grid grid-cols-2 gap-10">
-          {/* Button: Website */}
+          
           <div className="relative group">
             <div className="absolute inset-0 bg-black border-4 border-[#D9D9D9] transform transition-transform duration-300 group-hover:translate-x-2.5 group-hover:translate-y-3"></div>
             <div className="relative bg-[#D9D9D9] text-black border-black border-2 font-bold py-2 px-8 cursor-pointer text-center text-[1.5vw] ibm-plex-mono-bold hover:bg-black hover:text-white hover:border-white">
@@ -27,7 +27,7 @@ const Footer = () => {
             </div>
           </div>
 
-          {/* Button: Events */}
+          
           <div className="relative group">
             <div className="absolute inset-0 bg-black border-4 border-[#D9D9D9] transform transition-transform duration-300 group-hover:translate-x-2.5 group-hover:translate-y-3"></div>
             <div className="relative bg-[#D9D9D9] text-black border-black border-2 font-bold py-2 px-8 cursor-pointer text-center text-[1.5vw] ibm-plex-mono-bold hover:bg-black hover:text-white hover:border-white">
@@ -35,7 +35,7 @@ const Footer = () => {
             </div>
           </div>
 
-          {/* Button: Sp */}
+          
           <div className="relative group">
             <div className="absolute inset-0 bg-black border-4 border-[#D9D9D9] transform transition-transform duration-300 group-hover:translate-x-2.5 group-hover:translate-y-3"></div>
             <div className="relative bg-[#D9D9D9] text-black border-black border-2 font-bold py-2 px-8 cursor-pointer text-center text-[1.5vw] ibm-plex-mono-bold hover:bg-black hover:text-white hover:border-white">
@@ -43,7 +43,7 @@ const Footer = () => {
             </div>
           </div>
 
-          {/* Button: Home */}
+          
           <div className="relative group">
             <div className="absolute inset-0 bg-black border-4 border-[#D9D9D9] transform transition-transform duration-300 group-hover:translate-x-2.5 group-hover:translate-y-3"></div>
             <div className="relative bg-[#D9D9D9] text-black border-black border-2 font-bold py-2 px-8 cursor-pointer text-center text-[1.5vw] ibm-plex-mono-bold hover:bg-black hover:text-white hover:border-white">
@@ -53,15 +53,15 @@ const Footer = () => {
         </div>
       </div>
 
-      {/* Bottom Section with "Powered By" and Social Media Icons */}
+      
       <div className="h-[15vh] bg-black flex items-center justify-between px-20  text-white ">
-        {/* Powered By Section */}
+        
         <div className="flex items-center text-height ml-10">
           <span className="px-8 text-white text-lg text-3xl">Powered By :</span>
           <span className="font-bold text-2xl">musclemind</span>
         </div>
 
-        {/* Social Media Icons Section */}
+        
         <div className="flex space-x-11 mr-10">
         <Link href="/">
           <Image
