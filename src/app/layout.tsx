d727f9cb71062df6cce6f285e8ba170dbb68aebe
NishantGupt786 <nishantgupta2325@gmail.com>
2024-09-07 06:15:03 +0530

    mend

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ export const metadata: Metadata = {
   title: "CodeChef VIT | Gravitas'24",
   description:
     "The one-stop destination for all the tech enthusiasts out there. CodeChef VIT presents Cookoff 9.0 and Clueminati 2.0 co-hosted by Musclemind.",
-  
-  keywords: 
+
+  keywords:
     "CodeChef VIT, Gravitas 2024, Cookoff 9.0, Clueminati 2.0, tech events VIT, coding competitions VIT, programming events India, hackathons VIT, competitive programming, CodeChef events, tech festival VIT, VIT student events, technology conference, coding competition Gravitas, Cookoff VIT, Clueminati VIT, coding challenge UAE, technical fest VIT, VIT tech club, university coding events, tech enthusiasts India",
 
   openGraph: {
@@ -26,7 +26,6 @@ export const metadata: Metadata = {
   },
 };
 
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -37,14 +36,25 @@ export default function RootLayout({
       <head>
         <title>Codechef-VIT | Gravitas'24</title>
         <link rel="icon" type="image/x-icon" href="/favicon.ico"></link>
-        <meta name="description" content="The one-stop destination for all the tech enthusiasts out there. Codechef VIT presents Cookoff 9.0 and Clueminati 2.0 co-hosted by musclemind." />
-        <meta property="og:title" content="Join us for Cookoff 9.0 and Clueminati 2.0 at Gravitas'24. Your one-stop destination for all things tech and coding at VIT!" />
+        <meta
+          property="og:title"
+          content={"Codechef-VIT | Gravitas'24"}
+        />
+        <meta
+          name="og:description"
+          content={
+            "The one-stop destination for all the tech enthusiasts out there. Codechef VIT presents Cookoff 9.0 and Clueminati 2.0 co-hosted by musclemind."
+          }
+        />
         <meta property="og:image:type" content="image/png" />
         <meta property="og:image" content="https://imgur.com/cdbvOBA" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
         <meta property="og:image:alt" content="Codechef-VIT | Gravitas'24" />
-        <meta property="og:url" content={"https://gravitas24-landing-theta.vercel.app/"} />
+        <meta
+          property="og:url"
+          content={""}
+        />
         <meta property="og:site_name" content={"Codechef-VIT | Gravitas'24"} />
       </head>
       <body>
