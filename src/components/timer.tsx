fb07fbf1e89c236fc34597c8f31903a7bb5f5aa8
vaibhav700c <vaibhavjangid128@gmail.com>
2024-09-10 23:12:11 +0530

    made some final changes and brushed up some styling

--- src/components/timer.tsx
+++ src/components/timer.tsx
@@ -56,20 +56,20 @@ const Timer = ({ launchDate }: { launchDate: string }) => {
   
     return (
       <div className="flex flex-col justify-center items-center text-white bg-black sm:min-h-screen h-auto px-4 sm:px-0  sm:mx-0">
-        <h1 className="text-3xl sm:text-6xl font-bold mt-5 mb-10 mx-4 audiowide">
+        <h1 className="text-3xl sm:text-6xl font-bold  mb-10 mx-4 audiowide">
           The wait ends in
         </h1>
         <div className="grid grid-cols-3 sm:grid-cols-3 gap-0 sm:gap-0">
           {/* Days */}
-          <span className="flex flex-col justify-center items-center navinedemo bg-black text-red-500 text-4xl sm:text-9xl w-28 sm:w-64 py-6 sm:py-10 shadow-lg border-4 border-red-500 row-start-1 col-start-1">
+          <span className="flex flex-col justify-center items-center navinedemo bg-black text-[#D92626] text-4xl sm:text-9xl w-28 sm:w-64 py-6 sm:py-10 shadow-lg border-2 rounded  border-[#D92626] row-start-1 col-start-1">
             {timeLeft.days.toString().padStart(2, '0')}:
-            <small className="text-xl sm:text-3xl navinedemo font-semibold text-red-500 mt-2 sm:mt-3">
+            <small className="text-xl sm:text-3xl navinedemo font-semibold text-[#D92626] mt-2 sm:mt-3">
               Days
             </small>
           </span>
   
           {/* Hours */}
-          <span className="flex flex-col justify-center items-center navinedemo bg-black text-white text-4xl sm:text-9xl w-28 sm:w-64 py-6 sm:py-10 shadow-lg border-4 border-white row-start-1 col-start-3">
+          <span className="flex flex-col justify-center items-center navinedemo bg-black text-white text-4xl sm:text-9xl w-28 sm:w-64 py-6 sm:py-10 shadow-lg border-2 rounded border-white row-start-1 col-start-3">
             {timeLeft.hours.toString().padStart(2, '0')}:
             <small className="text-xl sm:text-3xl navinedemo font-semibold text-white mt-2 sm:mt-3">
               Hours
@@ -77,7 +77,7 @@ const Timer = ({ launchDate }: { launchDate: string }) => {
           </span>
   
           {/* Minutes */}
-          <span className="flex flex-col justify-center items-center navinedemo bg-black text-white text-4xl sm:text-9xl w-28 sm:w-64 py-6 sm:py-10 shadow-lg border-4 border-white row-start-2 col-start-2">
+          <span className="flex flex-col justify-center items-center navinedemo bg-black text-white text-4xl sm:text-9xl w-28 sm:w-64 py-6 sm:py-10 shadow-lg border-2 rounded  border-white row-start-2 col-start-2">
             {timeLeft.minutes.toString().padStart(2, '0')}:
             <small className="text-xl sm:text-3xl navinedemo font-semibold text-white mt-2 sm:mt-3">
               Minutes
@@ -85,7 +85,7 @@ const Timer = ({ launchDate }: { launchDate: string }) => {
           </span>
   
           {/* Seconds */}
-          <span className="flex flex-col justify-center items-center mb-8 sm:mb-16 navinedemo bg-black text-white text-4xl sm:text-9xl w-28 sm:w-64 py-6 sm:py-10 shadow-lg border-4 border-white row-start-3 col-start-1">
+          <span className="flex flex-col justify-center items-center mb-8 sm:mb-16 navinedemo bg-black text-white text-4xl sm:text-9xl w-28 sm:w-64 py-6 sm:py-10 shadow-lg border-2 rounded border-white row-start-3 col-start-1">
             {timeLeft.seconds.toString().padStart(2, '0')}
             <small className="text-xl sm:text-3xl navinedemo font-semibold text-white mt-2 sm:mt-3">
               Seconds
