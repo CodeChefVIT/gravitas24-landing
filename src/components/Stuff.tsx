1532babebe8ec084713847d9fa2ab956f9a3a63b
DhruvsChopra <dhruvsinghchopra70@gmail.com>
2024-09-11 20:49:04 +0530

    attempting to fix safarai issue part 3

--- src/components/Stuff.tsx
+++ src/components/Stuff.tsx
@@ -81,13 +81,13 @@ function CameraControls() {
     window.addEventListener('touchmove', handleTouchMove as unknown as EventListener);
     window.addEventListener('touchend', handleTouchEnd as unknown as EventListener);
 
-    (controls as OrbitControlsImpl)?.addEventListener('end', handleEnd);
+    (controls as OrbitControlsImpl).addEventListener('end' as any, handleEnd);
 
     return () => {
       window.removeEventListener('touchstart', handleTouchStart as unknown as EventListener);
       window.removeEventListener('touchmove', handleTouchMove as unknown as EventListener);
       window.removeEventListener('touchend', handleTouchEnd as unknown as EventListener);
-      (controls as OrbitControlsImpl)?.removeEventListener('end', handleEnd);
+      (controls as OrbitControlsImpl).removeEventListener('end' as any, handleEnd);
     };
   }, [camera, initialPosition, initialTarget]);
 
