cb10c05dfc40cb526fdfcb2adaa852099672b056
Aq <64064721+AqViolet@users.noreply.github.com>
2024-09-26 00:23:12 +0530

    Cook-Off Faq added (#43)
    * faq data updated
    
    * Update faqdata.ts
    
    * faq data update #3?
    
    ---------
    
    Co-authored-by: Abhinav G <56132559+Abh1noob@users.noreply.github.com>

--- src/lib/faqdata.ts
+++ src/lib/faqdata.ts
@@ -5,24 +5,56 @@ export interface FAQItem {
   
   export const faqs: FAQItem[] = [
     {
-      question: "Is Clueminati a team event?",
-      answer: "Yes, it is a team event with maximum of 4 people in a team. You can come with your own team or we can help you find one.",
+      question: "What is Cook-off-9.0",
+      answer: "A competitive coding event with increasing rounds of difficulty and elimination until there is an ultimate winner. It is an individual event which requires skill, problem solving and competence.",
     },
     {
-      question: "Does Clueminati feature CarryMinati?",
-      answer: "We don't have the streamer, BUT we do have a huge lineup of games for you :)",
+      question: "Will it be a team event?",
+      answer: "No, it is an individual participant event and not a team based event.",
     },
     {
-      question: "Do I need prior coding knowledge for Clueminati?",
-      answer: "No, not all your team need coding knowledge. Your diverse skills are the ticket to the prize",
+      question: "When is CookOff-9.0",
+      answer: "CookOff will be held on 29th September.",
     },
     {
-      question: "Why is ClueMinati special?",
-      answer: "Clueminati is not just your regular treasure hunt. You are signing up for the most exciting 4 hours of your life. \n **Secret: We have surprises guys!** ",
+      question: "Is there a registration fee?",
+      answer: "Yes, ₹200 (excluding GST).",
     },
     {
-      question: "Is Clueminati an offline event?",
-      answer: "It is a treasure hunt, that involves teams taking round around the campus and coding their way to clues at our venue.",
+      question: "How can I register?",
+      answer: "Registrations can be done on the graVITas portal itself.",
+    },
+    {
+      question: "What languages are supported?",
+      answer: "Java, C++, C, Python and other not so popular languages :)",
+    },
+    {
+      question: "Can we bring our laptops?",
+      answer: "Yes, you'll be required to bring your own devices.",
+    },
+    {
+      question: "Will CookOff be online?",
+      answer: "No, CookOff will be held offline in Anna Auditorium on 29th September.",
+    },
+    {
+      question: "How long is the event?",
+      answer: "The event is 8 hours long.",
+    },
+    {
+      question: "I have basic knowledge about coding. Can I still participate?",
+      answer: "Yes, the contest will have problems ranging from beginner to intermediate and further advanced levels. Anyone interested in programming with basic knowledge in any one programming language can participate.",
+    },
+    {
+      question: "Am I required to download any software?",
+      answer: "The event will be held on our portal. You don't need any external software.",
+    },
+    {
+      question: "Will I get a certificate?",
+      answer: "Yes, certificates will be provided.",
+    },
+    {
+      question: "If I still have some doubts, how do I get them resolved?",
+      answer: "Please contact us on any of our social handles to get your doubts resolved.",
     },
   ];
   
